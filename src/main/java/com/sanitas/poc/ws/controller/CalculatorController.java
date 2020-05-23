d9a257e0e4ea8b72a9d56ce2a408bbf7fbbb2945
lsaletta <lsaletta@vectoritcgroup.com>
2020-05-23 14:59:39 +0200

    Added logic in CalculatorController

--- src/main/java/com/sanitas/poc/ws/controller/CalculatorController.java
+++ src/main/java/com/sanitas/poc/ws/controller/CalculatorController.java
@@ -1,6 +1,9 @@
 package com.sanitas.poc.ws.controller;
 
+import com.sanitas.poc.core.service.CalculatorService;
 import com.sanitas.poc.model.dto.CalculatorRequest;
+import io.corp.calculator.TracerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,16 +11,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping(path = "/calculator")
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+    private TracerImpl tracer = new TracerImpl();
+
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
 
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<Double> calculator(
             @RequestBody CalculatorRequest request)
             throws Exception {
-        return new ResponseEntity<>(12D, HttpStatus.OK);
+        Double result = calculatorService.calculate(request);
+
+        tracer.trace(result);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
