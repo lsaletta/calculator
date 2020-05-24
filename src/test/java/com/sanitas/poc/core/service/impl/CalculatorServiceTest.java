dda4905860523e66a6c006c84a2888cee0085787
lsaletta <lsaletta@vectoritcgroup.com>
2020-05-24 15:59:42 +0200

    Fixed UT: CalculatorServiceTest

--- src/test/java/com/sanitas/poc/core/service/impl/CalculatorServiceTest.java
+++ src/test/java/com/sanitas/poc/core/service/impl/CalculatorServiceTest.java
@@ -1,19 +1,21 @@
 package com.sanitas.poc.core.service.impl;
 
 import com.sanitas.poc.core.exception.CalculationException;
+import com.sanitas.poc.core.service.Operation;
 import com.sanitas.poc.model.dto.Calculation;
 import com.sanitas.poc.model.dto.CalculatorRequest;
 import com.sanitas.poc.model.enums.EOperationType;
+import mockit.Expectations;
+import mockit.Injectable;
 import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 @RunWith(JMockit.class)
@@ -22,9 +24,26 @@ public class CalculatorServiceTest {
     @Tested
     private CalculatorServiceImpl calculatorService;
 
+    @Injectable
+    ApplicationContext applicationContextMock;
+
+    @Before
+    public void setup(){
+        Map<String, Object> operableServices = new HashMap<String, Object>() {{
+            put(EOperationType.SUM.toString(), new SumService());
+            put(EOperationType.SUBTRACT.toString(), new SubtractService());
+        }};
+
+        new Expectations() {{
+            applicationContextMock.getBeansWithAnnotation(Operation.class);
+            result = operableServices;
+        }};
+    }
+
 
     @Test
     public void calculate_when_is_sum_operation_return_parameters_sum() throws CalculationException {
+
         CalculatorRequest calculatorRequest = buildSumCalculatorRequest();
         Double result = calculatorService.calculate(calculatorRequest);
         Assert.assertEquals(Double.valueOf(36), result);
